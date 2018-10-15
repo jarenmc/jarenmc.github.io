dadb7e3c15d25e4234dfda4e2958687a6c2a5f08
jarenmc <44012883+jarenmc@users.noreply.github.com>
2018-10-15 01:28:59 -0400

    First website upload

---
+++ script.js
@@ -0,0 +1,74 @@
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display = 'block';
+}
+function revealMessage2(){
+	document.getElementById("hiddenMessage2").style.display = 'block';
+}
+function revealMessage3(){
+	document.getElementById("hiddenMessage3").style.display = 'block';
+}
+function revealMessage4(){
+	document.getElementById("hiddenMessage4").style.display = 'block';
+}
+function revealMessage5(){
+	document.getElementById("hiddenMessage5").style.display = 'block';
+}
+function revealMessage6(){
+	document.getElementById("hiddenMessage6").style.display = 'block';
+}
+function revealMessage7(){
+	document.getElementById("hiddenMessage7").style.display = 'block';
+}
+function revealMessage8(){
+	document.getElementById("hiddenMessage8").style.display = 'block';
+}
+function revealMessage9(){
+	document.getElementById("hiddenMessage9").style.display = 'block';
+}
+function revealMessage10(){
+	document.getElementById("hiddenMessage10").style.display = 'block';
+}
+function revealMessage11(){
+	document.getElementById("hiddenMessage11").style.display = 'block';
+}
+function revealMessage12(){
+	document.getElementById("hiddenMessage12").style.display = 'block';
+}
+function revealMessage13(){
+	document.getElementById("hiddenMessage13").style.display = 'block';
+}function revealMessage14(){
+	document.getElementById("hiddenMessage14").style.display = 'block';
+}
+function revealMessage15(){
+	document.getElementById("hiddenMessage15").style.display = 'block';
+}
+function revealMessage16(){
+	document.getElementById("hiddenMessage16").style.display = 'block';
+}
+function revealMessage17(){
+	document.getElementById("hiddenMessage17").style.display = 'block';
+}
+function revealMessage18(){
+	document.getElementById("hiddenMessage18").style.display = 'block';
+}
+function revealMessage19(){
+	document.getElementById("hiddenMessage19").style.display = 'block';
+
+}function revealMessage20(){
+	document.getElementById("hiddenMessage20").style.display = 'block';
+}
+function revealMessage21(){
+	document.getElementById("hiddenMessage21").style.display = 'block';
+}
+function revealMessage22(){
+	document.getElementById("hiddenMessage22").style.display = 'block';
+}
+function revealMessage23(){
+	document.getElementById("hiddenMessage23").style.display = 'block';
+}
+function revealMessage24(){
+	document.getElementById("hiddenMessage24").style.display = 'block';
+}
+function revealMessage25(){
+	document.getElementById("hiddenMessage25").style.display = 'block';
+}
